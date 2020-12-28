f42d00d305ff6fd89348ecb981ddf8566581df92
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-12-29 01:31:12 +0600

    sign up activity

--- app/src/main/java/com/imran/android/java_firebaseemailauthenticationsdk/SignUpActivity.java
+++ app/src/main/java/com/imran/android/java_firebaseemailauthenticationsdk/SignUpActivity.java
@@ -1,8 +1,11 @@
 package com.imran.android.java_firebaseemailauthenticationsdk;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,6 +16,7 @@ public class SignUpActivity extends AppCompatActivity {
     private EditText email;
     private EditText password;
     private EditText repeatPassword;
+    private ProgressBar progressBar;
 
     private FirebaseAuth firebaseAuth;
 
@@ -25,7 +29,8 @@ public class SignUpActivity extends AppCompatActivity {
         email = (EditText) findViewById(R.id.editTextEmailSignUp);
         password = (EditText) findViewById(R.id.editTextPasswordSignUp);
         repeatPassword = (EditText) findViewById(R.id.editTextRePasswordSignUp);
-
+        progressBar = (ProgressBar) findViewById(R.id.progressBarSignUp);
+        
         firebaseAuth = FirebaseAuth.getInstance();
     }
 
@@ -39,5 +44,39 @@ public class SignUpActivity extends AppCompatActivity {
         String emailInput = email.getText().toString().trim();
         String passwordInput = password.getText().toString().trim();
         String repeatPasswordInput = repeatPassword.getText().toString().trim();
+
+        if (TextUtils.isEmpty(nameInput)) {
+            showToast("User name is empty");
+            return;
+        }
+
+        if (TextUtils.isEmpty(emailInput)) {
+            showToast("Email is empty");
+            return;
+        }
+
+        if (passwordInput.isEmpty()) {
+            showToast("Password is empty");
+            return;
+        }
+
+        if (repeatPasswordInput.isEmpty()) {
+            showToast("Repeat password is empty");
+            return;
+        }
+
+        if (passwordInput != repeatPasswordInput) {
+            showToast("passwords not matched");
+            return;
+        }
+
+        if (passwordInput.length() < 8) {
+            showToast("Password is too short");
+            return;
+        }
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
