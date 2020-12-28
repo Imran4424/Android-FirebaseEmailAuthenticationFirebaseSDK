4b0b1f38e03c5338939b604d40a98f8ffcbef3ed
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-12-29 00:42:03 +0600

    sign up

--- app/src/main/java/com/imran/android/java_firebaseemailauthenticationsdk/SignUpActivity.java
+++ app/src/main/java/com/imran/android/java_firebaseemailauthenticationsdk/SignUpActivity.java
@@ -1,14 +1,24 @@
 package com.imran.android.java_firebaseemailauthenticationsdk;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SignUpActivity extends AppCompatActivity {
+    private EditText name;
+    private EditText email;
+    private EditText password;
+    private EditText repeatPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        name = (EditText) findViewById(R.id.editTextNameSignUp);
+        email = (EditText) findViewById(R.id.editTextEmailSignUp);
+        password = (EditText) findViewById(R.id.editTextPasswordSignUp);
+        repeatPassword = (EditText) findViewById(R.id.editTextRePasswordSignUp);
     }
 }
