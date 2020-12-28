4bc288c15e93bf13aa98fb20c41f249e5e71f5d1
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-12-29 02:15:49 +0600

    sign up activity

--- app/src/main/java/com/imran/android/java_firebaseemailauthenticationsdk/SignInActivity.java
+++ app/src/main/java/com/imran/android/java_firebaseemailauthenticationsdk/SignInActivity.java
@@ -3,12 +3,14 @@ package com.imran.android.java_firebaseemailauthenticationsdk;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SignInActivity extends AppCompatActivity {
     private EditText email;
     private EditText password;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +19,13 @@ public class SignInActivity extends AppCompatActivity {
 
         email = (EditText) findViewById(R.id.editTextEmailSignIn);
         password = (EditText) findViewById(R.id.editTextPasswordSignIn);
+        progressBar = (ProgressBar) findViewById(R.id.progressBarSignIn);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        progressBar.setVisibility(View.GONE);
     }
 
     public void signInForm(View view) {
