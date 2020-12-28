8d6b90994ffac74e77efdd1e98bd54e2f3101110
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-12-29 00:54:30 +0600

    signed in

--- app/src/main/java/com/imran/android/java_firebaseemailauthenticationsdk/SignedInActivity.java
+++ app/src/main/java/com/imran/android/java_firebaseemailauthenticationsdk/SignedInActivity.java
@@ -1,14 +1,25 @@
 package com.imran.android.java_firebaseemailauthenticationsdk;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SignedInActivity extends AppCompatActivity {
+    private TextView name;
+    private TextView email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signed_in);
+
+        name = (TextView) findViewById(R.id.textNameSigned);
+        email = (TextView) findViewById(R.id.textEmailSigned);
+    }
+
+    public void signOutFinal(View view) {
+        
     }
 }
